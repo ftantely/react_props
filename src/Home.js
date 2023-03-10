ce19aa5aa5ea47946d84f57223b20895ea7fa863
tantely <ftantely@gmail.com>
2023-03-10 12:27:45 -0700

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+  return (
+    <div>
+      {blogs.map((blog) => (
+        <div>
+          <h2>{blog.title}</h2>
+          <p>{blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
