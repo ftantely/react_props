abd0e1e5e521aaa1ffbd2732c8dc4d42de778748
tantely <ftantely@gmail.com>
2023-03-10 13:22:10 -0700

    3_Destructure

--- src/BlogList.js
+++ src/BlogList.js
@@ -3,11 +3,12 @@ import React from "react";
 It will not render, we will get an error:"src/BlogList.js
  Line 6:8:  'blogs' is not defined  no-undef"
  */
-const BlogList = (props) => {
-  const test = props.data_1;
+const BlogList = ({ data_1 }) => {
+  // const test = props.data_1;
+
   return (
     <div>
-      {test.map((blog) => (
+      {data_1.map((blog) => (
         <div>
           <h2>{blog.title}</h2>
           <p>{blog.author}</p>
